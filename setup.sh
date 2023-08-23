c2b9eb1eca99dde83794ae956a1bb8c73804c796
PrazTobie <tobie.praz@heig-vd.ch>
2023-08-23 09:30:27 +0200

    Laravel Sail

---
+++ setup.sh
@@ -0,0 +1,2 @@
+docker run --rm --interactive --tty -v ./webApp:/app -u $(id -u ${USER}):$(id -g ${USER}) composer:2.5 install --dev
+docker run --rm --interactive --tty -v ./webApp:/var/www/html -u $(id -u ${USER}):$(id -g ${USER}) --entrypoint "/bin/bash" php:8.2 -c "cd /var/www/html && cp .env.example .env && php artisan key:generate"
