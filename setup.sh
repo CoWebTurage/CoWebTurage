ed07f9f8281437299ff2499f4b92f39d4fc4a47d
PrazTobie <tobie.praz@heig-vd.ch>
2023-08-23 14:32:25 +0200

    Fix setup to use absolute paths

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-docker run --rm --interactive --tty -v ./webApp:/app -u $(id -u ${USER}):$(id -g ${USER}) composer:2.5 install --dev
-docker run --rm --interactive --tty -v ./webApp:/var/www/html -u $(id -u ${USER}):$(id -g ${USER}) --entrypoint "/bin/bash" php:8.2 -c "cd /var/www/html && cp .env.example .env && php artisan key:generate"
+
+docker run --rm --interactive --tty -v $(realpath ./webApp):/app -u $(id -u ${USER}):$(id -g ${USER}) composer:2.5 install --dev
+docker run --rm --interactive --tty -v $(realpath ./webApp):/var/www/html -u $(id -u ${USER}):$(id -g ${USER}) --entrypoint "/bin/bash" php:8.2 -c "cd /var/www/html && cp .env.example .env && php artisan key:generate"
