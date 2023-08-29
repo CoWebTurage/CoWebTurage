a7f044d6c0377018ee10fc035a1f3ab7dff20579
Léo <leo.zmoos@heig-vd.ch>
2023-08-29 18:42:47 +0200

    logout fix

--- webApp/resources/views/layouts/navigation.blade.php
+++ webApp/resources/views/layouts/navigation.blade.php
@@ -38,9 +38,14 @@
                         @csrf
                         <a class="button button-white button-sm"
                            href="{{ route('profile.edit') }}">{{ $user->firstname." ".$user->lastname }}</a>
-                        <a class="button button-white button-sm"
-                           href="{{ route('logout') }}">{{ __('Log Out') }}</a>
 
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <x-dropdown-link :href="route('logout')"
+                                             onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </form>
                     @else
                         <a class="button button-white button-sm"
                            href="{{ route('login') }}">{{ __('Log In') }}</a>
